a0cb6384c5bf6c6d0fdec7a1cbad1382a173f0cb
kayanswq <108351310+kayanswq@users.noreply.github.com>
2022-06-29 14:57:31 +0800

    pull

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,5 +13,6 @@ public class App
         System.out.println("1eqw1231");
         System.out.println("master change");
         System.out.println("hot fix change");
+        System.out.println("从远程库github拉起项目");
     }
 }
